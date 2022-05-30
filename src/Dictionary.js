6c1240ea35494a8bb9510b65b4e19d1af7a87bdd
Itzel Gil <79197169+itzelgil@users.noreply.github.com>
2022-05-30 11:14:43 +0200

    adding results

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,13 +1,24 @@
 import React, { useState } from "react";
+import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
+
+  function handleResponse(response) {
+    console.log(response.data[0]);
+    setResults(response.data[0]);
+  }
 
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
   }
+
+  let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(handleResponse);
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
@@ -22,6 +33,7 @@ export default function Dictionary() {
           onChange={handleKeywordChange}
         />
       </form>
+      <Results results={results} />
     </div>
   );
 }
