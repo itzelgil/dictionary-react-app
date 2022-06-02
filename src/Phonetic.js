551d4bfac5ca7d2de04d090069e5f6e5c2cc54ff
Itzel Gil <79197169+itzelgil@users.noreply.github.com>
2022-06-02 14:18:48 +0200

    added phonetic component

---
+++ src/Phonetic.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Phonetic(props) {
+  console.log(props.phonetic);
+  if (props.phonetic) {
+    return (
+      <div className="phonetic">
+        <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
+          Listen
+        </a>
+        <br />
+        {props.phonetic.text}
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
