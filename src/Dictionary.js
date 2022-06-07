5f35f66c44a694488ac409f57d6d1aae7cb1ff8a
Itzel Gil <79197169+itzelgil@users.noreply.github.com>
2022-06-07 11:44:10 +0200

    added function to load a keyword by default

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -6,33 +6,54 @@ import "./Dictionary.css";
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("sunrise");
   let [results, setResults] = useState(null);
+  let [loaded, setLoaded] = useState(false);
 
   function handleResponse(response) {
-    console.log(response.data[0]);
+    // console.log(response.data[0]);
     setResults(response.data[0]);
   }
 
-  function search(event) {
-    event.preventDefault();
+  function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     console.log(apiUrl);
     axios.get(apiUrl).then(handleResponse);
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
-  return (
-    <div className="Dictionary">
-      <form onSubmit={search}>
-        <input
-          type="search"
-          placeholder="Search your word"
-          className=""
-          onChange={handleKeywordChange}
-        />
-      </form>
-      <Results results={results} />
-    </div>
-  );
+
+  function load() {
+    setLoaded(true);
+    search();
+  }
+
+  if (loaded) {
+    return (
+      <div className="Dictionary">
+        <section className="form">
+          <form onSubmit={handleSubmit}>
+            <input
+              type="search"
+              placeholder="Search your word"
+              className=""
+              onChange={handleKeywordChange}
+            />
+          </form>
+          <div className="hint">
+            Suggested words: sunset, wine, yoga, plant...
+          </div>
+        </section>
+        <Results results={results} />
+      </div>
+    );
+  } else {
+    load();
+    return "Loading...";
+  }
 }
--- src/Results.js
+++ src/Results.js
@@ -1,24 +1,29 @@
 import React from "react";
 import Meaning from "./Meaning";
 import Phonetic from "./Phonetic";
+import "./Results.css";
 
 export default function Results(props) {
   if (props.results) {
     return (
       <div className="Results">
-        <h2 className="word">{props.results.word}</h2>
-        {props.results.phonetics.map(function (phonetic, index) {
-          return (
-            <div key={index}>
-              <Phonetic phonetic={phonetic} />
-            </div>
-          );
-        })}
+        <section className="">
+          <h2 className="word">{props.results.word}</h2>
+          {props.results.phonetics.map(function (phonetic, index) {
+            return (
+              <div key={index}>
+                <Phonetic phonetic={phonetic} />
+              </div>
+            );
+          })}
+        </section>
         {props.results.meanings.map(function (meaning, index) {
           return (
-            <div key={index}>
-              <Meaning meaning={meaning} />
-            </div>
+            <section className="">
+              <div key={index}>
+                <Meaning meaning={meaning} />
+              </div>
+            </section>
           );
         })}
       </div>
